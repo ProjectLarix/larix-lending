229c423c1bd9a07c9cff4dbd3cbe3614198f17c8
jack <jack93@gmail.com>
2022-06-23 18:29:49 +0800

    Delete entrypoint file that useless

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,6 @@
 // Export current sdk types for downstream users building with a different sdk version
 pub use solana_program;
 
-pub mod entrypoint;
 pub mod error;
 pub mod instruction;
 pub mod math;
